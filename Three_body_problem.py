3873c31f9abf4d077602bb60fe96815367b97b90
pablo.rpp <pablo.ramirezpuetas.96@protonmail.com>
2022-08-04 14:27:57 +0200

    Main

---
+++ Three_body_problem.py
@@ -0,0 +1,80 @@
+
+from bodyClass import *
+from RKClass import *
+import numpy as np
+import sys
+import os
+import vpython as vp
+from colorama import Fore, Style
+from tqdm import tqdm
+
+
+N = 100000
+a = 0
+b = 1000
+
+#locations
+q1 = np.array([0.5, 0.1, 0.3])
+q2 = np.array([0, 0.5, 0])
+q3 = np.array([0, 0, 0.5])
+
+#momentums
+p1 = np.array([0, 0, 0])
+p2 = np.array([0, 0, 0])
+p3 = np.array([0, 0, 0])
+
+#mass
+mass = [1E5, 2E5, 5E5]
+
+body1 = Body(q1, p1, mass[0])
+body2 = Body(q2, p2, mass[1])
+body3 = Body(q3, p3, mass[2])
+
+RK = rungeKutta(body1, body2, body3, N, a, b)
+
+del(p1, p2, p3, q1, q2, q3, mass, N, a, b)
+
+
+
+def Bosco():
+    global G
+    nullIncrement = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+
+
+    try:
+        os.mkdir('./Simulation_Data')
+    except FileExistsError:
+        pass
+
+    file = open('Simulation_Data/positions_simulation.txt', 'w')
+    print("Calculando trayectorias...")
+    for j in tqdm(range(RK.numIter)):
+        RK.wrapKL()
+        #k1, l1
+        RK.kCalculus(0, nullIncrement)
+        RK.lCalculus(0, nullIncrement)
+        #k2, l2
+        RK.kCalculus(1, 0.5*Rk.h*self.wrappedK)
+        RK.lCalculus(1, 0.5*Rk.h*self.wrappedL)
+        #k3, l3
+        RK.kCalculus(2, 0.5*Rk.h*self.wrappedK)
+        RK.lCalculus(2, 0.5*Rk.h*self.wrappedL)
+        #k4, l4
+        RK.kCalculus(3, (Rk.h-1)*self.wrappedK)
+        RK.lCalculus(3, (Rk.h-1)*self.wrappedL)
+
+        RK.rkLocation()
+        RK.rkMomentum()
+
+        file.write(str(RK.bodies[0].location[0]) + ',' + str(RK.bodies[0].location[1])+ ','+ str(RK.bodies[0].location[2])+ ';')
+
+
+        file.write(str(RK.bodies[1].location[0]) + ',' + str(RK.bodies[1].location[1])+ ','+ str(RK.bodies[1].location[2])+ ';')
+
+
+        file.write(str(RK.bodies[2].location[0]) + ',' + str(RK.bodies[2].location[1])+ ','+ str(RK.bodies[2].location[2])+ '\n')
+
+
+    file.close()
+
+Bosco()
